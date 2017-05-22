f60a2c30e4e39e1ff93fb34615468e4b7de48339
SergeantSarcasm <quin.broussard@gmail.com>
2017-05-22 10:45:26 -0500

    Add files via upload

--- shaders/lib/utility.glsl
+++ shaders/lib/utility.glsl
@@ -29,11 +29,26 @@ mat2 rotate(float rad) {
 	);
 }
 
+#define fsqrt(x) intBitsToFloat(0x1FBD1DF5 + (floatBitsToInt(x) >> 1)) // Error of 1.42%
+#define finversesqrt(x) intBitsToFloat(0x5F33E79F - (floatBitsToInt(x) >> 1)) // Error of 1.62%
+
 float facos(float x) { // Under 3% error
   float ax = abs(x);
   float res = -0.156583 * ax + PI * 0.5;
-  res *= sqrt(1.0 - ax);
+  res *= fsqrt(1.0 - ax);
   return x >= 0 ? res : PI - res;
+}
+
+float flength(vec2 x) {
+	return fsqrt(dot(x, x));
+}
+
+float flength(vec3 x) {
+	return fsqrt(dot(x, x));
+}
+
+float flength(vec4 x) {
+	return fsqrt(dot(x, x));
 }
 
 #define toLinear(x) powf(x, 2.2)
